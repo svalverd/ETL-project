e4fd80ce7cc4408c88157a6950fd7ce3a65d03f7
svalverd <40702702+svalverd@users.noreply.github.com>
2020-03-16 23:47:59 -0400

    Add files via upload

---
+++ ETL-project/Schema.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE mass_shootings_2018 (
+Incident_Date VARCHAR,
+States VARCHAR,
+City_Or_County VARCHAR,
+Address VARCHAR,
+Killings INT,
+Injuries INT,
+Operations VARCHAR
+);
+
+
+CREATE TABLE mass_shootings_2019 (
+Incident_Date VARCHAR,
+States VARCHAR,
+City_Or_County VARCHAR,
+Address VARCHAR,
+Killings INT,
+Injuries INT,
+Operations VARCHAR
+);
+
+
